24dab7dec1b8773b6fd5030f15ebdcea7ec78a40
Eric Hunsberger <erichuns@gmail.com>
2014-03-29 15:51:56 -0400

    Got check-numpy working, looks for packages

--- check-numpy.py
+++ check-numpy.py
@@ -1,4 +1,6 @@
 import os
+import re
+import subprocess
 import numpy
 
 numpy_root = os.path.dirname(numpy.__file__)
@@ -8,3 +10,13 @@ core_files = sorted(os.listdir(numpy_core))
 print "##### core files #####"
 for filename in core_files:
     print filename
+
+
+process = subprocess.Popen("dpkg --get-selections".split(),
+                           stdout=subprocess.PIPE)
+output = process.communicate()[0]
+
+print "##### numpy packages #####"
+for line in output.split('\n'):
+    if line.find("numpy") >= 0:
+        print line
